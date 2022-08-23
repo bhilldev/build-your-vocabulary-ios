0e5db4f62cc4111acc4a9f65cb77bfa4cd537edf
Brandon Hill <bhilldev@outlook.com>
2022-08-23 12:53:25 -0500

    Added a table view

--- BuildYourVocabulary/Controller/VocabularyViewController.swift
+++ BuildYourVocabulary/Controller/VocabularyViewController.swift
@@ -11,9 +11,10 @@ class VocabularyViewController: UIViewController, UISearchBarDelegate {
     
     @IBOutlet weak var mySearchBar: UISearchBar!
     
+    @IBOutlet weak var tableView: UITableView!
     var wordManager = WordManager()
     
-    
+    var wordArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,12 +22,17 @@ class VocabularyViewController: UIViewController, UISearchBarDelegate {
         // Do any additional setup after loading the view.
         mySearchBar.delegate = self
         wordManager.delegate = self
-        
+        tableView.dataSource = self
         
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        wordArray.append(mySearchBar.text!)
+//        for word in wordArray {
+//            print(word)
+//        }
         wordManager.fetchWordDefinition(word: mySearchBar.text!)
+        tableView.reloadData()
     }
 }
 
@@ -38,5 +44,21 @@ extension VocabularyViewController: WordManagerDelegate {
     }
     func didFailWithError(error: Error) {
         print("error is \(error)")
+    }
+}
+
+extension VocabularyViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped me")
+    }
+}
+extension VocabularyViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return wordArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = wordArray[indexPath.row]
+        return cell
     }
 }
